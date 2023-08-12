daa5879308e47a821d807d664d68932c9789d5b1
Saumya Vatsa <saumyavatsa2108@gmail.com>
2023-08-12 14:43:31 +0530

    Drawing different shapes

--- main.py
+++ main.py
@@ -9,11 +9,23 @@ myScreen = Screen()
 myTurtle.shape('turtle')
 myTurtle.color('red')
 
-n = 4
-while n >0 :
-    myTurtle.forward(200)
-    myTurtle.right(90)
-    n-=1
+def shape(angle, color, n):
+    myTurtle.color(color)
+    while n > 0:
+        myTurtle.forward(100)
+        myTurtle.right(angle)
+        n -= 1
 
+myTurtle.forward(100)
+myTurtle.right(120)
+myTurtle.forward(100)
+myTurtle.right(120)
+shape(90, 'yellow', 4)
+shape(72, 'pink', 5)
+shape(60,'blue',6)
+shape(51.4, 'red', 7)
+shape(45, 'purple', 8)
+shape(40, 'green', 9)
+shape(36, 'black', 10)
 
 myScreen.exitonclick()
